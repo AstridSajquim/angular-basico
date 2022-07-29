d9c399aa7b2bc3861f93e48ee90f243d8d085a42
aSajquim <asajquim@gyt.com.gt>
2022-07-29 16:52:51 -0600

    agregar valores desde imput

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-
+interface Personaje {
+  nombre:String;
+  poder:Number;
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-
-  agregar(event:any){
-    event.preventDefault();
-    console.log("Hey!!!")
+  personajes:Personaje[]=[
+    {
+      nombre:"Goku",
+      poder:9005,
+    },
+    {
+      nombre:"Vegeta",
+      poder:7500,
+    }
+  ]
+  nuevo:Personaje = {
+    nombre:'',
+    poder:0
   }
-
+  agregar(){
+    if(this.nuevo.nombre.trim().length ===0 ){ return;}
+    this.personajes.push(this.nuevo)
+    this.nuevo={
+      nombre:"",
+      poder:0,
+    }
+    console.log(this.nuevo)
+  }
 }
